2e0537ba17d8b3438fbd50b44d3648bafadb710f
DARK <david92005999@gmail.com>
2023-01-19 02:24:06 +0100

    Added whole code and lang data

---
+++ src/langs.ts
@@ -0,0 +1,259 @@
+const langs: any = {
+    bat: {
+        single: '::',
+        multiStart: '::',
+        multiEnd: '::'
+    },
+    c: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    csharp: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    cpp: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    clojure: {
+        single: ';;',
+        multiStart: ';;',
+        multiEnd: ';;'
+    },
+    coffeescript: {
+        single: '#',
+        multiStart: '###',
+        multiEnd: '###'
+    },
+    css: {
+        single: '/*',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    "cuda-cpp": {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    dart: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    fsharp: {
+        single: '//',
+        multiStart: '(*',
+        multiEnd: '*)'
+    },
+    go: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    groovy: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    handlebars: {
+        single: '{{!}}',
+        multiStart: '{{!',
+        multiEnd: '}}'
+    },
+    hlsl: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    html: {
+        single: '<!---->',
+        multiStart: '<!--',
+        multiEnd: '-->'
+    },
+    ignore: {
+        single: '#',
+        multiStart: '#',
+        multiEnd: '#'
+    },
+    ini: {
+        single: ';',
+        multiStart: ';',
+        multiEnd: ';'
+    },
+    java: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    javascript: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    javascriptreact: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    jsonc: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    julia: {
+        single: '#',
+        multiStart: '#=',
+        multiEnd: '=#'
+    },
+    latex: {
+        single: '%',
+        multiStart: '%',
+        multiEnd: '%'
+    },
+    less: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    lua: {
+        single: '--',
+        multiStart: '--[[',
+        multiEnd: ']]'
+    },
+    makefile: {
+        single: '#',
+        multiStart: '#',
+        multiEnd: '#'
+    },
+    markdown: {
+        single: '<!---->',
+        multiStart: '<!--',
+        multiEnd: '-->'
+    },
+    "objective-c": {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    "objective-cpp": {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    perl: {
+        single: '#',
+        multiStart: '=',
+        multiEnd: '=cut'
+    },
+    perl6: {
+        single: '#',
+        multiStart: '#',
+        multiEnd: '#'
+    },
+    php: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    powershell: {
+        single: '#',
+        multiStart: '<#',
+        multiEnd: '#>'
+    },
+    properties: {
+        single: '#',
+        multiStart: '#',
+        multiEnd: '#'
+    },
+    jade: {
+        single: '//-',
+        multiStart: '//-',
+        multiEnd: '//-'
+    },
+    python: {
+        single: '#',
+        multiStart: '"""',
+        multiEnd: '"""'
+    },
+    r: {
+        single: '#',
+        multiStart: '#',
+        multiEnd: '#'
+    },
+    razor: {
+        single: '<!---->',
+        multiStart: '<!--',
+        multiEnd: '-->'
+    },
+    ruby: {
+        single: '#',
+        multiStart: '=begin',
+        multiEnd: '=end'
+    },
+    rust: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    scss: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    shellscript: {
+        single: '#',
+        multiStart: ": '",
+        multiEnd: "'"
+    },
+    sql: {
+        single: '--',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    swift: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    tex: {
+        single: '%',
+        multiStart: '%',
+        multiEnd: '%'
+    },
+    typescript: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    typescriptreact: {
+        single: '//',
+        multiStart: '/*',
+        multiEnd: '*/'
+    },
+    vb: {
+        single: "'",
+        multiStart: "'",
+        multiEnd: "'"
+    },
+    xml: {
+        single: '<!---->',
+        multiStart: '<!--',
+        multiEnd: '-->'
+    },
+    xsl: {
+        single: '<!---->',
+        multiStart: '<!--',
+        multiEnd: '-->'
+    },
+    yaml: {
+        single: '#',
+        multiStart: '#',
+        multiEnd: '#'
+    },
+}
+
+export = langs
